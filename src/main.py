51fbe7ae7d63037d169cbe97cb8f7fbc20f04d0e
jlucasa <amen.jaredl@gmail.com>
2021-12-13 09:46:47 -0700

    duuhhhh

--- src/main.py
+++ src/main.py
@@ -150,8 +150,8 @@ def layer_vis(feats, num_epoch, output_container):
             if i == 8:
                 row = 1
             
-            axes[i, row].axis('off')
-            axes[i, row].imshow(filter, cmap='nipy_spectral')
+            axes[row, i % 8].axis('off')
+            axes[row, i % 8].imshow(filter, cmap='nipy_spectral')
 
         activation_container.pyplot(fig)
 
